4ebdfd9f9e4fc3255184cf12f948564b42d8d6e4
joao mendonca <117647563+joaomendonca1234@users.noreply.github.com>
2024-08-29 16:22:52 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o nome da galáxia em que vivemos?",
+        alternativas: [
+            {
+                texto: "A) Andrômeda",
+                afirmacao: [
+                    "B) Via Láctea – galáxia em que vivemos."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Via Láctea.",
+                afirmacao: [
+                    "B) Via Láctea – galáxia em que vivemos."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quem escreveu 'Dom Quixote'?",
+        alternativas: [
+            {
+                texto: "A) William Shakespeare",
+                afirmacao: [
+                    "B) Miguel de Cervantes – escreveu 'Dom Quixote'.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Miguel de Cervantes",
+                afirmacao: [
+                    "B) Miguel de Cervantes – escreveu 'Dom Quixote'."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o elemento químico representado pelo símbolo 'O'?",
+        alternativas: [
+            {
+                texto: "A) Óxido",
+                afirmacao: [
+                    "B) Oxigênio – elemento representado pelo símbolo 'O'."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Oxigênio",
+                afirmacao: [
+                    "B) Oxigênio – elemento representado pelo símbolo 'O'."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Em que ano o homem pisou na Lua pela primeira vez?",
+        alternativas: [
+            {
+                texto: "A) 1969",
+                afirmacao: [
+                    "A) 1969 – ano em que o homem pisou na Lua pela primeira vez."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) 1967",
+                afirmacao: [
+                    "A) 1969 – ano em que o homem pisou na Lua pela primeira vez."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quem pintou a Mona Lisa?",
+        alternativas: [
+            {
+                texto: "A) Michelangelo",
+                afirmacao: [
+                    "B) Leonardo da Vinci – pintou a Mona Lisa."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Leonardo da Vinci",
+                afirmacao: [
+                    "B) Leonardo da Vinci – pintou a Mona Lisa."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o maior planeta do sistema solar?",
+        alternativas: [
+            {
+                texto: "A) Saturno",
+                afirmacao: [
+                    "B) Júpiter – maior planeta do sistema solar."
+                ],
+            },
+            {
+                texto: "B) Júpiter",
+                afirmacao: [
+                    "B) Júpiter – maior planeta do sistema solar."
+                ],
+            },
+        ]
+    },
+];
